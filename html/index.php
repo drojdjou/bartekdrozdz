8b7316170995ed9d6cf26549fb91bdd743358271
Bartek Drozdz <bartek@everyday3d.com>
2013-06-24 13:57:59 -0700

    Added smarty engine and some basic data to work with

--- html/index.php
+++ html/index.php
@@ -1,1 +1,19 @@
-<?php header("Location: steps/03_wireframe"); ?>
+<?php
+
+define('SMARTY_DIR', '../php-lib/smarty/');
+require_once(SMARTY_DIR . 'Smarty.class.php');
+
+$data = json_decode(file_get_contents('data/main.json'));
+
+$postA = json_decode(file_get_contents('data/post-a.json'));
+$postB = json_decode(file_get_contents('data/post-a.json'));
+$postC = json_decode(file_get_contents('data/post-a.json'));
+
+$smarty = new Smarty();
+
+$smarty->assign('data', $data);
+$smarty->assign('posts', array($postA, $postB, $postC));
+
+$smarty->display('templates/main.html');
+
+?>
