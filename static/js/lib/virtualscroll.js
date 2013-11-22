1fa5a4248a0580ea249f0de87f0886e7aa9c7f63
Bartek Drozdz <bartek@everyday3d.com>
2013-11-21 16:31:13 -0800

    New VirtualScroll

--- static/js/lib/virtualscroll.js
+++ static/js/lib/virtualscroll.js
@@ -1,102 +1,153 @@
-window.VirtualScroll = (function(document) {
+var VirtualScroll = (function(document) {
 
 	var vs = {};
 
 	var numListeners, listeners = [], initialized = false;
 
-	var touchStartX, touchStartY, touchMult = 2;
-	var mozMult = -10;
+	var touchStartX, touchStartY;
+
+	// How many pixels to move with each key press
+	var keyStep = 120;
+	var bodyTouchAction;
+
+	var hasWheelEvent = 'onwheel' in document;
+	var hasMouseWheelEvent = 'onmousewheel' in document;
+	var hasTouch = 'ontouchstart' in document;
+	var hasTouchWin = navigator.msMaxTouchPoints && navigator.msMaxTouchPoints > 1;
+	var hasPointer = !!window.navigator.msPointerEnabled;
+	var hasKeyDown = 'onkeydown' in document;
 
 	var event = {
 		y: 0,
 		x: 0,
 		deltaX: 0,
-		deltaY: 0
+		deltaY: 0,
+		originalEvent: null
 	};
 
-	/**
-	 *	Add a javascript function that will receive feedback everytime 
-	 *  the user activates the scoll or moved his finger on touch devices:
-	 *
-	 *	VirtualScroll.addEventListener(function(e) {
-	 *		// e.x = the global scroll y
-	 *		// e.y = the global scroll x
-	 *		// e.deltaX = the amount of movement on x axis since last call
-	 *		// e.deltaY = the amount of movement on y axis since last call
-	 *  });
-	 *	
-	 *	To make a scrool with a basic ease do this:
-	 *
-	 *	var curScroll = 0, targetScroll = 0;
-	 *	var minScroll = 0, maxScroll = /height of some element or any other value that makes sense/
-	 *
-	 *	VirtualScroll.addEventListener(function(e) {
-	 *		targetScroll += e.deltaY;
-	 		targetScroll = Math.min(targetScroll, maxScroll);
-	 		targetScroll = Math.max(targetScroll, minScroll);
-	 *	});
-	 *
-	 *	function draw() {
-	 *		requestAnimationFrame(draw);
-	 *		// Adjust this value (0.1) for different easing value. 1 = no easing, 0.000001 - loooots of easing
-	 *		curScroll += (targetScroll - curScroll) * 0.1;
-	 *		// Typically you will want to move the div in the opposite directior, hence * -1
-	 *		someDiv.style.transform = "translateY(" + (curScroll * -1) + "px)";
-	 *	}
-	 */
 	vs.addEventListener = function(f) {
 		if(!initialized) initListeners(); 
 		listeners.push(f);
 		numListeners = listeners.length;
 	}
 
-	/**
-	 *	Firefox will not fire mouse wheel events when mouse is over an iframe. 
-	 *	Workaround - listen to mouse wheel event in the iframe and forward it here.
-	 */
-	vs.invokeFirefox = function(e) {
-		event.y += e.detail * mozMult;
-		event.deltaY = e.detail * mozMult;
-		notify();
+	vs.removeEventListener = function(f) {
+		listeners.splice(f, 1);
+		numListeners = listeners.length;
+		if(numListeners <= 0) destroyListeners();
 	}
 
-	var notify = function() {
+	var notify = function(e) {
+		event.x += event.deltaX;
+		event.y += event.deltaY;
+		event.originalEvent = e;
+
 		for(var i = 0; i < numListeners; i++) {
 			listeners[i](event);
 		}
 	}
 
-	var initListeners = function() {
+	var onWheel = function(e) {
+		// In Chrome and in Firefox (at least the new one)
+		event.deltaX = e.wheelDeltaX || e.deltaX * -1;
+		event.deltaY = e.wheelDeltaY || e.deltaY * -1;
 
-		var wheelFunc = function(e, dy) {
-			e.preventDefault();
-			event.y += dy;
-			event.deltaY = dy;
-			notify();
+		notify(e);
+	}
+
+	var onMouseWheel = function(e) {
+		// In Safari, IE and in Chrome if 'wheel' isn't defined
+		event.deltaX = (e.wheelDeltaX) ? e.wheelDeltaX : 0;
+		event.deltaY = (e.wheelDeltaY) ? e.wheelDeltaY : e.wheelDelta;
+
+		notify(e);	
+	}
+
+	var onTouchStart = function(e) {
+		var t = (e.targetTouches) ? e.targetTouches[0] : e;
+		touchStartX = t.pageX;	
+		touchStartY = t.pageY;
+	}
+
+	var onTouchMove = function(e) {
+		// e.preventDefault(); // < This needs to be managed externally
+		var t = (e.targetTouches) ? e.targetTouches[0] : e;
+
+		event.deltaX = t.pageX - touchStartX;
+		event.deltaY = t.pageY - touchStartY;
+		
+		touchStartX = t.pageX;
+		touchStartY = t.pageY;
+
+		notify(e);
+	}
+
+	var onKeyDown = function(e) {
+		// 37 left arrow, 38 up arrow, 39 right arrow, 40 down arrow
+		event.deltaX = event.deltaY = 0;
+		switch(e.keyCode) {
+			case 37:
+				event.deltaX = -keyStep;
+				break;
+			case 39:
+				event.deltaX = keyStep;
+				break;
+			case 38:
+				event.deltaY = -keyStep;
+				break;
+			case 40:
+				event.deltaY = keyStep;
+				break;
 		}
 
-		document.addEventListener("mousewheel", function(e) {
-			wheelFunc(e, e.wheelDeltaY || e.wheelDelta);		
-		});
+		notify(e);
+	}
 
-		document.addEventListener("DOMMouseScroll", function(e) {
-			wheelFunc(e, e.detail * mozMult);		
-		});
+	var initListeners = function() {
+		if(hasWheelEvent) document.addEventListener("wheel", onWheel);
+		if(hasMouseWheelEvent) document.addEventListener("mousewheel", onMouseWheel);
 
-		document.addEventListener("touchstart", function(e) {
-			var t0 = e.targetTouches[0];
-			touchStartX = t0.pageX;
-			touchStartY = t0.pageY;
-		});
+		if(hasTouch) {
+			document.addEventListener("touchstart", onTouchStart);
+			document.addEventListener("touchmove", onTouchMove);
+		}
+		
+		if(hasPointer && hasTouchWin) {
+			bodyTouchAction = document.body.style.msTouchAction;
+			document.body.style.msTouchAction = "none";
+			document.addEventListener("MSPointerDown", onTouchStart, true);
+			document.addEventListener("MSPointerMove", onTouchMove, true);
+		}
 
-		document.addEventListener("touchmove", function(e) {
-			var t0 = e.targetTouches[0];
-			wheelFunc(e, (t0.pageY - touchStartY) * touchMult);
-			touchStartY = t0.pageY;
-		});
+		if(hasKeyDown) document.addEventListener("keydown", onKeyDown);
 
 		initialized = true;
 	}
 
+	var destroyListeners = function() {
+		if(hasWheelEvent) document.removeEventListener("wheel", onWheel);
+		if(hasMouseWheelEvent) document.removeEventListener("mousewheel", onMouseWheel);
+
+		if(hasTouch) {
+			document.removeEventListener("touchstart", onTouchStart);
+			document.removeEventListener("touchmove", onTouchMove);
+		}
+		
+		if(hasPointer && hasTouchWin) {
+			document.body.style.msTouchAction = bodyTouchAction;
+			document.removeEventListener("MSPointerDown", onTouchStart, true);
+			document.removeEventListener("MSPointerMove", onTouchMove, true);
+		}
+
+		if(hasKeyDown) document.removeEventListener("keydown", onKeyDown);
+
+		initialized = false;
+	}
+
 	return vs;
 })(document);
+
+
+
+
+
