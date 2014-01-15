7d5d90e15a21195fbd792bd7906f7bc5d0ad44de
Bartek Drozdz <bartek@everyday3d.com>
2014-01-15 15:23:56 -0800

    Added GA to every page, even non-JS

--- static/src/main.js
+++ static/src/main.js
@@ -8,15 +8,10 @@ window.addEventListener('load', function() {
 
 	Site(rawData);
 
-	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-	ga('create', 'UA-46599235-1', 'bartekdrozdz.com');
-	
 	Application.on(MSG.ROUTE, function(e) {
-		// console.log("Route", e);
-		ga('send', 'pageview');
+		setTimeout(function() {
+			if(ga) ga('send', 'pageview');
+		}, 10);
 	});
 
 });
