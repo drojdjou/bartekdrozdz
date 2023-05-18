cd7db1863c57aec917e16ff0d887fdbb2ce0103b
Bartek <bartek@everyday3d.com>
2023-05-18 12:25:24 -0700

    Upgrading the styling and image

--- dev/main.js
+++ dev/main.js
@@ -1,7 +1,6 @@
 let e = document.querySelector("#email");
 
 e.addEventListener("click", () => {
-    console.log("ac");
     let m = "mailto:" + window.atob("YmFydGVrQGV2ZXJ5ZGF5M2QuY29t");
     window.location.href = m;
 });
